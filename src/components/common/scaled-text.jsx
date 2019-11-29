f82b46b31be8e216af3a954e99ea0c0c1b11828f
mc-eyc <53546505+mc-eyc@users.noreply.github.com>
2019-11-29 13:45:34 +1000

    Cleanup of Edge detection.

--- src/components/common/scaled-text.jsx
+++ src/components/common/scaled-text.jsx
@@ -48,7 +48,7 @@ export default function ScaledText(props) {
     const [textRenderer, setTextRenderer] = useState(null);
     const [isEdge, setIsEdge] = useState(false);
 
-    // Setup the Edge and Firefox state detection
+    // Setup the Edge detection so that we can apply the specific SVG attributes needed for it.
     useEffect(() => {
         const browserName = bowser.getParser(window.navigator.userAgent).getBrowserName();
         setIsEdge(browserName === "Microsoft Edge");
@@ -76,7 +76,7 @@ export default function ScaledText(props) {
 
     return (
         <StyledText className="scaled-text" width="100%" height="100%" viewBox={viewBox}>
-            <text x="50%" y="50%" dy={isEdge ? "1ex" : "0%"} textAnchor="middle" alignmentBaseline="central" dominantBaseline={"central"} ref={textRef}>
+            <text x="50%" y="50%" dy={isEdge ? "1ex" : "0%"} textAnchor="middle" alignmentBaseline="central" dominantBaseline="central" ref={textRef}>
                 {str}
             </text>
         </StyledText>
